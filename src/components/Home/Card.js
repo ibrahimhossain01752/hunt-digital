8c5b616f94d8bfc21228451050b4a02abdeb47bd
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-12-08 09:35:28 +0600

    card added

---
+++ src/components/Home/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+    </div>
+ </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      </div>
+   </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+     </div>
+    </div>
+</div>
+    );
+};
+
+export default Card;
